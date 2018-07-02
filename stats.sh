9dc54a0e105d0a801fb4e3c09d72ba2d8b9aa13c
Leonids Nahodkins <leonids.nahodkins@accenture.com>
2018-07-02 19:13:08 +0300

    hello world

---
+++ stats.sh
@@ -0,0 +1,24 @@
+urlsfile=$1
+logfiles=`ls *.dat`
+
+for logfile in $logfiles
+do
+total=`cat $logfile | wc -l`
+echo
+echo "====================="
+echo "$logfile"
+echo "====================="
+printf "%10s %10s %s\n" "count" "percents" "endpont" 
+printf "%10s %10s %s\n" "$total" "100%" "ALL" 
+OLDIFS=$IFS
+IFS=$'\n'
+for i in $(cat ${urlsfile})
+do
+i=$(echo $i | tr -d '\n' | tr -d '\r')
+count=`grep $i $logfile | wc -l`
+percents=$((100*$count/$total))"%"
+printf "%10s %10s %s\n" "$count" "$percents" "$i"
+done
+IFS=$OLDIFS
+echo "====================="
+done
