0d0314755fd5d8ec026b28a70ed87a6f3835577b
LeonidsNahodkins <leonids.nahodkins@accenture.com>
2018-07-30 18:08:58 +0300

    Update parse.sh

--- parse.sh
+++ parse.sh
@@ -1,23 +1,46 @@
 service=$1
 logname=$2
 
-cat ${logname} | grep -Eo "(GET|POST|PUT|DELETE).* HTTP" | cut -d " " -f1,2 > ${service}.dat
+# Two possible date formats: "DD/MMM/YYYY:HH:mm:ss" OR "YYYY-MM-DDTHH:mm:ss"
 
-#Get list of all days when Timestams like this [2018-07-11T06:53:55+00:00]
-days=$(cat ${logname} | grep -Eo [0-9]+-[0-9]+-[0-9]+T | uniq)
-#Get one day before last, in order to get the full day stats
-day=$(echo "${days}" | tail -n 2 | head -n 1 | grep -Eo [0-9]+-[0-9]+-[0-9]+)
+# Get one day before last, in order to get the full day stats
+date_pattern1="[0-9]{1,2}/[A-Za-z]{3}+/[0-9]{4}"
+date_pattern2="[0-9]{4}+-[0-9]{2}-[0-9]{2}"
+hour_pattern1="DAY:HOUR:[0-9]+:[0-9]+"
+hour_pattern2="DAYTHOUR:[0-9]+:[0-9]+"
+
+count1=$(cat ${logname} | grep -Eo "${date_pattern1}" | wc -l)
+count2=$(cat ${logname} | grep -Eo "${date_pattern2}" | wc -l)
+
+if [[ $count1 -gt $count2 ]]
+then
+echo "Format1 DD/MMM/YYYY:HH:mm:ss"
+date_pattern=$date_pattern1
+hour_pattern=$hour_pattern1
+else
+echo "Format2 YYYY-MM-DDTHH:mm:ss"
+date_pattern=$date_pattern2
+hour_pattern=$hour_pattern2
+fi
+
+days=$(cat ${logname} | grep -Eo "${date_pattern}" | uniq)
+echo "$days"
+day=$(echo "${days}" | tail -n 2 | head -n 1 | grep -Eo $date_pattern)
+
 echo "Counting stats hourly for this day $day"
+echo "date_pattern = ${date_pattern}"
+echo "hour_pattern = ${hour_pattern}"
 
 for i in {1..24} 
 do
 hour=$(printf "%02d" $i)
-thetime=$(printf "%sT%s" ${day} ${hour})
-count=$(cat  ${logname} | grep "${thetime}" | wc -l)
+thetime=$(echo ${hour_pattern} | sed "s~DAY~${day}~g" | sed "s~HOUR~${hour}~g")
+
+count=$(cat  ${logname} | grep -E "${thetime}" | wc -l)
 if [ $count -gt 0 ]
 then
-cat  ${logname} | grep "$thetime" |  grep -Eo "(GET|POST|PUT|DELETE).* HTTP" | cut -d " " -f1,2 > ${service}_${hour}.dat
-count=$(cat  ${service}_${hour}.dat | wc -l)
+cat  ${logname} | grep -E "${thetime}" |  grep -Eo "(GET|POST|PUT|DELETE).* HTTP" | cut -d " " -f1,2 > ${service}_${hour}.dat
+count=$(cat ${service}_${hour}.dat | wc -l)
 fi
 echo "$thetime count=$count"
 done
